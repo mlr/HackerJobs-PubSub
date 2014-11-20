bb2d8fbcea79954a6e6ba6445e7c457f04ee6a51
Scott Hasbrouck <scott@ginkgotree.com>
2014-11-20 15:41:24 -0500

    Adds 4 jobs sites
    YC, Smash, StackOverflow, Github

--- app.js
+++ app.js
@@ -11,31 +11,83 @@ var cheerio = require('cheerio');
 var _ = require('underscore');
 var app = express();
 
+//JSON of scrape sites with elements
+var scrapes = [
+	{
+		'url': 'http://jobs.smashingmagazine.com/all/programming',
+		'titles': 'article.entry h2',
+		'engagements': 'article.entry .entry-type',
+		'locations': 'article.entry .entry-company',
+		'company': '',
+		'links': 'entry-list li a',
+		'linkprefix': '',
+		'timeout': 300,
+		'pageindex': '',
+		'maxpage': ''
+	},
+	{
+		'url': 'https://news.ycombinator.com/jobs',
+		'titles': '.title a',
+		'engagements': '',
+		'locations': '.title .comhead',
+		'company': '',
+		'links': '.title a',
+		'linkprefix': '',
+		'timeout': 300,
+		'pageindex': '',
+		'maxpage': ''
+	},
+	{
+		'url': 'http://careers.stackoverflow.com/jobs',
+		'titles': '.-title a',
+		'engagements': '',
+		'locations': '.-employer',
+		'company': '',
+		'links': '.-title a',
+		'linkprefix': 'http://careers.stackoverflow.com',
+		'timeout': 300,
+		'pageindex': 'pg',
+		'maxpage': '9'
+	},
+	{
+		'url': 'https://jobs.github.com/positions',
+		'titles': '.title a',
+		'engagements': '',
+		'locations': 'span.location',
+		'company': '.company',
+		'links': 'h4 a',
+		'linkprefix': 'https://jobs.github.com',
+		'timeout': 300,
+		'pageindex': '',
+		'maxpage': ''
+	}
+];
+
 app.get('/', function(req, res) {
 	res.type('text/plain');
-	var url = 'http://jobs.smashingmagazine.com/all/programming';
-	var jobs = {};
+	var jobs = [];
+	var siteindex = 3;
 
-	request(url, function(err, resp, html) {
+	request(scrapes[siteindex]['url'], function(err, resp, html) {
 		if(!err) {
 			var $ = cheerio.load(html);
 			var titles = [];
 			var engagements = [];
 			var locations = [];
 			var links = [];
-			$('article.entry h2').each(function(i) {
+			$(scrapes[siteindex]['titles']).each(function(i) {
 				titles.push({'title': $(this).text()});
 			});
-			$('article.entry .entry-type').each(function(i) {
+			$(scrapes[siteindex]['engagements']).each(function(i) {
 				engagements.push({'engagement': $(this).text()});
 			});
-			$('article.entry .entry-company').each(function(i) {
+			$(scrapes[siteindex]['locations']).each(function(i) {
 				locations.push({'location': $(this).text()});
 			});
-			$('.entry-list li a').each(function(i) {
-				links.push({'link': $(this).attr('href')});
+			$(scrapes[siteindex]['links']).each(function(i) {
+				links.push({'link': scrapes[siteindex]['linkprefix'] + $(this).attr('href')});
 			});
-			jobs = _.zip(titles, engagements, locations, links);
+			jobs = jobs.concat(_.zip(titles, engagements, locations, links));
 		}
 		res.json(jobs);
 		res.end();
